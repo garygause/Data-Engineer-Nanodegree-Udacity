c50ea0ce239a833b1efa8a7427cec7b4a6760df7
Gary Gause <gary@jennylife.com>
2019-04-08 15:30:44 -0700

    add log data etl

--- projects/Project1-DataModellingInPostgreSQL/sql_queries.py
+++ projects/Project1-DataModellingInPostgreSQL/sql_queries.py
@@ -11,11 +11,11 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 songplay_table_create = ("""
 CREATE TABLE songplays (
     songplay_id serial PRIMARY KEY, 
-    start_time TIME NOT NULL, 
+    start_time timestamp NOT NULL, 
     user_id integer NOT NULL, 
     level VARCHAR(255) NOT NULL, 
-    song_id VARCHAR(255) NOT NULL, 
-    artist_id VARCHAR(255) NOT NULL, 
+    song_id VARCHAR(255), 
+    artist_id VARCHAR(255), 
     session_id integer NOT NULL, 
     location VARCHAR(255), 
     user_agent VARCHAR(255)
@@ -38,7 +38,7 @@ CREATE TABLE songs (
     title VARCHAR(255) NOT NULL, 
     artist_id VARCHAR(255) NOT NULL, 
     year integer NOT NULL, 
-    duration float(5) NOT NULL
+    duration double precision NOT NULL
 )
 """)
 
@@ -54,7 +54,7 @@ CREATE TABLE artists (
 
 time_table_create = ("""
 CREATE TABLE time (
-    start_time TIME, 
+    start_time timestamp, 
     hour integer NOT NULL, 
     day integer NOT NULL, 
     week integer NOT NULL, 
@@ -67,35 +67,32 @@ CREATE TABLE time (
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+INSERT INTO songplays (start_time, user_id, level, session_id, location, user_agent, song_id, artist_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
+INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
 song_table_insert = ("""
-INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)
+INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
 artist_table_insert = ("""
-INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s)
+INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
 """)
 
 
 time_table_insert = ("""
+INSERT INTO time (day, hour, month, start_time, week, weekday, year) VALUES (%s, %s, %s, %s, %s, %s, %s)
 """)
 
 # FIND SONGS
 
 song_select = ("""
-SELECT * FROM songs WHERE title = "%s"
+SELECT songs.song_id, artists.artist_id FROM songs INNER JOIN artists ON (songs.artist_id = artists.artist_id) WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s
 """)
 
 # QUERY LISTS
-# order is important here, make sure tables are created before tables with foreign keys 
-create_table_queries = [time_table_create, artist_table_create, song_table_create, user_table_create, songplay_table_create]
-
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-
-# original order in template
-#create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-#drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
